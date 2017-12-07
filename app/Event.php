1f90c6ecc24cfd689fe5a1b484f4b31739d5d162
Gaëtan Menat <gmenat@Kyxis.intra.insa-cvl.fr>
2017-12-07 22:22:37 +0100

    mod(Event): Create Eloquent model of Event

--- app/Event.php
+++ app/Event.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $table = 'event';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'begin_date', 'orga_id', 'visibility',
+    ];
+
+    public function posts() {
+    	return $this->hasMany('App\EventPost', 'event_id');
+    }
 }
