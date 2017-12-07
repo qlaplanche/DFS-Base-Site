2941136285125be363dac0f56d9638d8a434647d
Jeterim <jeterim2@gmail.com>
2017-12-08 00:12:29 +0100

    feat (EventViews): Add index view of events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Faire condition si il y a event en cours return view viewEvent avec l'id de l'envet en cours
+        //Si pas d'event en cours return view myEvents
+        return view('event.index', ['currents' => [], 'futures' => [], 'pasts' => []]);
+
+
+    }
 }
