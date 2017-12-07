acfb7ec9da36ce4b84be8752f38eebec41435ee8
Gaëtan Menat <gmenat@Kyxis.intra.insa-cvl.fr>
2017-12-07 22:22:37 +0100

    mod(Event): Create Eloquent model of Event

--- database/migrations/2017_12_07_185430_create_events_table.php
+++ database/migrations/2017_12_07_185430_create_events_table.php
@@ -15,7 +15,17 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('place')->nullable();
+            $table->string('photo')->nullable();
+            $table->dateTime('begin_date');
+            $table->dateTime('end_date')->nullable();
+            $table->integer('orga_id')->unsigned();
+            $table->boolean('visibility');
             $table->timestamps();
+
+            $table->foreign('orga_id')->references('id')->on('users');
         });
     }
 
