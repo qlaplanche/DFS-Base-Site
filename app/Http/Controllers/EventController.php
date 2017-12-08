a704724610d6c9b336a63749bcd3dae37d40cdb7
Jeterim <jeterim2@gmail.com>
2017-12-08 05:54:47 +0100

    feat (notify): First step of notify

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Event;
+use App\ProblemHistory;
 
 use Illuminate\Http\Request;
 
@@ -35,5 +36,38 @@ class EventController extends Controller
     public function getEvent($eventid)
     {
         return view('event.view', ['event' => Event::findOrFail($eventid)]);
+    }
+
+    /**
+     * Create new item
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createProblem(Request $request)
+    {
+        $this->validate($request, [
+            'description' => 'required|max:200',
+            'longitude' => 'required|max:50',
+            'latitude' => 'required|max:50',
+            'situation' => 'required',
+        ]);
+
+        $problem = new ProblemHistory();
+        $problem->description = $request->input("description");
+        $problem->longitude = $request->input("longitude");
+        $problem->latitude = $request->input("latitude");
+
+        
+
+
+        $message = 'There was an error';
+        if ($problem->save()) {
+            $message = 'Problem successfully created!';
+            session(['alert-class' => 'warning']);
+            session(['alert-msg' => 'Je suis perdu']);
+            session(['alert-btn' => 'Terminée']);
+        }
+
+        return redirect()->route('item.index')->with('message', $message);
     }
 }
