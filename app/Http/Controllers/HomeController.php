f5fcafbefaff05bbf6ca021dca9f59e97fac1be4
Gaëtan Menat <gaetan.menat@gmail.com>
2017-12-08 06:52:02 +0100

    fix(index): fix index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,6 +37,6 @@ class HomeController extends Controller
             return redirect()->route('event.view', ['id' => $event->id]);
         }
 
-        return view('event.index');
+        return redirect()->route('events');
     }
 }
