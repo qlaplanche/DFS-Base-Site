cee4cacda4a7f01581f0a1f6abb3e96a3c06a689
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-12-08 03:35:50 +0100

    redirect to my events in navbar

--- database/seeds/ParticipantTableSeeder.php
+++ database/seeds/ParticipantTableSeeder.php
@@ -12,8 +12,8 @@ class ParticipantTableSeeder extends Seeder
     public function run()
     {
         DB::table('participants')->insert([
-        	'user' => 1,
-            'event' => 1,
+        	'user_id' => 1,
+            'event_id' => 1,
         ]);
     }
 }
