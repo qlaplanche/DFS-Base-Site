3a75d8921a6f507823e032586f06911cfe4ed174
Yann Caumartin <yann.caumartin@insa-cvl.fr>
2017-12-08 04:11:53 +0100

    Seeds table for ProblemHistory table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             EventTableSeeder::class,
+	    ProblemHistoryTableSeeder::class,
             ]);
     }
 }
