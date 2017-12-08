98a27da9c80d51201b4ab988d2613cc17945aeb6
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-12-08 01:10:27 +0100

    Ajout vue view event

--- routes/web.php
+++ routes/web.php
@@ -59,18 +59,18 @@ Route::prefix('event')->group(function () {
     ]);
 });
 
-Route::get('/user/edit/{user_id}', [
+Route::get('/event/edit/{user_id}', [
     'uses' => 'EventController@getEditEvent',
     'as' => 'event.edit'
 ]);
 
 //Post
-Route::post('/user/edit/{event_id}', [
+Route::post('/event/edit/{event_id}', [
     'uses' => 'EventController@editEvent',
     'as' => 'event.edit'
 ]);
 
-Route::get('/user/delete/{event_id}', [
+Route::get('/event/delete/{event_id}', [
     'uses' => 'EventController@deleteEvent',
     'as' => 'event.delete'
 ]);
